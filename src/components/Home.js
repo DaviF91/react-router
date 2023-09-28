5bee248f7578dda4a61f78b3a7771b71d22f87b9
Davi Ferreira <daviferreiraaew@gmail.com>
2023-09-28 17:40:19 -0300

    projetop react-router

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div id='app'>
+      <h1>Explore o universo</h1>
+      <button>Quero Saber Mais</button>
+    </div>
+  );
+};
+
+export default Home;
